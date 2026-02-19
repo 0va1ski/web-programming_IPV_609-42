101922d41d8419ae7b3f54c64cb351822f8b9299
ovalski <ily1na.poly@yandex.tu>
2026-02-19 13:31:23 +0500

    Lab2 - добавлены ссылки и представления (изменены views.py и urls.py)

--- laboratorka/laboratorka/urls.py
+++ laboratorka/laboratorka/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from laboratorka import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homepage),
+    path('about/', views.about, kwargs={"company_name":"FooBar", "company_phone":"88005553535"}),
 ]
