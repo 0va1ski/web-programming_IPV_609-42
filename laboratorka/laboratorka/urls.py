1b625b1f5a83b68d225a747613f05fe083f78b83
ovalski <ily1na.poly@yandex.tu>
2026-02-19 14:56:12 +0500

    Lab3 - созданы и заполненны шаблоны страниц(изменены views.py, urls.py, templates)

--- laboratorka/laboratorka/urls.py
+++ laboratorka/laboratorka/urls.py
@@ -22,5 +22,5 @@ from laboratorka import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
-    path('about/', views.about, kwargs={"company_name":"FooBar", "company_phone":"88005553535"}),
+    path('about/', views.about),
 ]
